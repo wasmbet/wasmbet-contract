51058b9999b32283c069a1f771f359f2c4f3e91b
wasmbet <wasmbet@gmail.com>
2020-08-26 04:29:01 +0900

    working

--- src/state.rs
+++ src/state.rs
@@ -5,12 +5,26 @@ use cosmwasm_std::{CanonicalAddr, Storage};
 use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
 
 pub static CONFIG_KEY: &[u8] = b"config";
+//pub static ROOM_KEY: &[u8] = b"room";
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Room_State {
+    pub room_owner: CanonicalAddr,
+    pub seed : Vec<u8>,
+    pub start_time: u64,
+    pub entropy: Vec<u8>,
+    pub Prediction_number: i32,
+    pub Lucky_Number: i32,
+    pub position: String,
+    pub results: String,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
-    pub owner: CanonicalAddr,
+    pub contract_owner: CanonicalAddr,
+    pub player_room: Vec<Room_State>,
 }
+
 
 pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
     singleton(storage, CONFIG_KEY)
@@ -19,3 +33,11 @@ pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
 pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
     singleton_read(storage, CONFIG_KEY)
 }
+
+//pub fn room<S: Storage>(storage: &mut S) -> Singleton<S, State> {
+//    singleton(storage, ROOM_KEY)
+//}
+
+//pub fn room_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
+//    singleton_read(storage, ROOM_KEY)
+//}
